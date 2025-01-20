b294617f1fe2acc8bdf51bcefeaa3d5699df35ca
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-20 15:51:53 -0500

    style: heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="flex justify-center">
+      <div className="title my-10">
+        <h1 className="text-6xl font-black tracking-widest text-white">
+          TYPER
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
