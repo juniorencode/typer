1be35ff295d5ed963e4ea6dbc8bf6ed532cc7b89
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-21 17:13:59 -0500

    style: padding and gap normalize

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ const App = () => {
   const typedCharacters = typed.split('');
 
   return (
-    <div className="flex flex-col items-center max-w-screen-lg mx-auto">
-      <div className="my-10">
+    <div className="flex flex-col items-center gap-4 mx-auto pt-4 px-2 max-w-screen-lg backdrop-blur-xl">
+      <div className="py-10 w-full text-center rounded-xl border bg-black/60 border-white/10">
         <h1 className="text-8xl text-shadow font-nexa tracking-widest select-none text-white">
           {'</typer>'}
         </h1>
       </div>
-      <div className="px-2 py-10 w-full">
-        <div className="w-full rounded-xl overflow-hidden border border-white/20">
+      <div className="w-full">
+        <div className="w-full rounded-xl overflow-hidden border border-white/10">
           <div className="flex items-center justify-between px-4 w-full h-12 bg-black bg-opacity-80">
             <div className="flex items-center gap-2">
               <div className="w-3 h-3 rounded-full bg-red-500"></div>
@@ -42,24 +42,24 @@ const App = () => {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-5 gap-4 px-2 w-full text-sm tracking-wider text-neutral-400">
-        <div className="col-span-1 px-2 py-4 rounded-xl backdrop-blur-xl bg-black/60">
+      <div className="grid grid-cols-5 gap-4 w-full text-sm tracking-wider text-neutral-400">
+        <div className="col-span-1 px-2 py-4 rounded-xl border backdrop-blur-xl bg-black/60 border-white/10">
           <div>words per minute</div>
           <div className="text-4xl font-bold text-white">46</div>
         </div>
-        <div className="col-span-1 px-2 py-4 rounded-xl backdrop-blur-xl bg-black/60">
+        <div className="col-span-1 px-2 py-4 rounded-xl border backdrop-blur-xl bg-black/60 border-white/10">
           <div>characters per minute</div>
           <div className="text-4xl font-bold text-white">230</div>
         </div>
-        <div className="col-span-1 px-2 py-4 rounded-xl backdrop-blur-xl bg-black/60">
+        <div className="col-span-1 px-2 py-4 rounded-xl border backdrop-blur-xl bg-black/60 border-white/10">
           <div>accuracy</div>
           <div className="text-4xl font-bold text-white">98%</div>
         </div>
-        <div className="col-span-1 px-2 py-4 rounded-xl backdrop-blur-xl bg-black/60">
+        <div className="col-span-1 px-2 py-4 rounded-xl border backdrop-blur-xl bg-black/60 border-white/10">
           <div>time</div>
           <div className="text-4xl font-bold text-white">2m 36s</div>
         </div>
-        <div className="col-span-1 px-2 py-4 rounded-xl backdrop-blur-xl bg-black/60">
+        <div className="col-span-1 px-2 py-4 rounded-xl border backdrop-blur-xl bg-black/60 border-white/10">
           <div>mistkes</div>
           <div className="text-4xl font-bold text-white">3</div>
         </div>
